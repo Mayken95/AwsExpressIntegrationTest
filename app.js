66d8bcf4c757c32661bec53095b1ac39e900f2e6
Mayken <mayu-satu@hotmail.com>
2023-07-16 17:30:33 -0500

    project prueba

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
+
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World! Att. Mayken Salavarría Tutivén'));
+app.set('view engine','pug');
+app.set('views',__dirname + '/views');  //tambien se puede poner app.set('views', './views'); 
+app.get('/', (request,response)=>{
+   // response.send('Hello from server');//para enviar un texto plano
+   const name = request.query.name;
+   const edad = request.query.edad;
+   const nacionalidad =  request.query.nacionalidad;
+   const genero = request.query. genero;
+   const estatura = request.query.estatura;
+   const genero_text = (genero=='f')?'Femenino':'Masculino';
+   const resp ={title: 'Hey' , message:'Hello!', name, edad, 
+   nacionalidad, gender: genero_text, estatura}
+   response.render('pug/index',resp);
+});
+app.listen(3000,()=>{
+    console.log('Ready in port 3000!');
+});
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
